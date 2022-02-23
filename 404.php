62dfb41659fdd59f9afb2a154d7624ca9d41c847
Danishcar Casimir <e2051961@cmaisonneuve.qc.ca>
2022-02-23 17:40:29 -0500

    semaine 5 avencement exercice2 en class

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header() ?>
+<main class="site__main">
+    <section class="erreur-404">
+        <h1>ERROR 404</h1>
+        <h2>PAGE NOT FOUND</h2>
+        <P>TRY SOMETHING ELSE LOOOOOOOOOOSER</P>
+    </section>
+</main>
+<?php get_footer() ?>
--- header.php
+++ header.php
@@ -14,8 +14,17 @@
 </head>
 <body  <?php body_class("site"); ?>>
 <header class="site__header">
-    <h1 class="header__titre"><?php bloginfo('name'); ?></h1>
+    <h1 class="header__titre"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
     <h2 class="header__description"><?php bloginfo('description'); ?></h2>
+    <div class="util">
+        <div class="util__menu">
+        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zm-.464 5.535a1 1 0 10-1.415-1.414 3 3 0 01-4.242 0 1 1 0 00-1.415 1.414 5 5 0 007.072 0z" clip-rule="evenodd"></path></svg>
+        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zm-.464 5.535a1 1 0 10-1.415-1.414 3 3 0 01-4.242 0 1 1 0 00-1.415 1.414 5 5 0 007.072 0z" clip-rule="evenodd"></path></svg>
+        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zm-.464 5.535a1 1 0 10-1.415-1.414 3 3 0 01-4.242 0 1 1 0 00-1.415 1.414 5 5 0 007.072 0z" clip-rule="evenodd"></path></svg>
+        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zm-.464 5.535a1 1 0 10-1.415-1.414 3 3 0 01-4.242 0 1 1 0 00-1.415 1.414 5 5 0 007.072 0z" clip-rule="evenodd"></path></svg>
+        </div>
+    <?php get_search_form() ?>
+    </div>
 </header>
 <section class="site__barre">
     <input type="checkbox"  id="chk-burger">
