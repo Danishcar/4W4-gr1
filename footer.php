c704e6aeb3f0a5424ef2f41eb95c8e83b5b3274e
Danishcar Casimir <Danishcarcasimir25@gmail.com>
2022-04-22 09:19:50 -0400

    réarangement du footer

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <footer class="site__footer">
 <div class="site__footer__colonne">
-    <section class="site__footer__colonne__adresse"><?php get_sidebar('footer_colonne_1'); ?></section>
-    <section class="site__footer__colonne__article"><?php get_sidebar('footer_colonne_2'); ?></section>
-    <section class="site__footer__colonne__lien"><?php get_sidebar('footer_colonne_3'); ?></section>
+    <section class="site__footer__colonne__adresse"><?php get_sidebar('pied_page_colonne_1'); ?></section>
+    <section class="site__footer__colonne__article"><?php get_sidebar('pied_page_colonne_2'); ?></section>
+    <section class="site__footer__colonne__lien"><?php get_sidebar('pied_page_colonne_3'); ?></section>
 </div>
 
 <div class="site__footer__rangee">
@@ -13,9 +13,9 @@
                                 "container_class" => "site__footer__rangee__menu",
                                 "menu_class" => "menu_footer")); 
     ?>
-    <section class="site__footer__rangee__sociaux"><?php get_sidebar('footer_ligne_1'); ?></section>
+    <section class="site__footer__rangee__sociaux"><?php get_sidebar('pied_page_ligne_1'); ?></section>
     <section class="site__footer__rangee__droits">&copy; Collège de Maisonneuve - Tous droits réservés</section>
-    <section class="site__footer__rangee__auteur">Thème réalisé par Danishcar Casimir</section>
+    <section class="site__footer__rangee__auteur">Thème réalisé par Florence Lemieux Cayer</section>
 </div>
 
 </body>
