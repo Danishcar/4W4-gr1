66929d450f5b445c84452ad353d964bbe961ddf2
Danishcar Casimir <Danishcarcasimir25@gmail.com>
2022-04-22 09:46:10 -0400

    modification de la bar de recherche

--- search.php
+++ search.php
@@ -9,23 +9,17 @@
  * @package cidw-4w4
  */
 ?>
-<?php get_header() ?>;
+<?php get_header(); ?>
 <main class="site__main">
-    <h1 class="site__main__titre">
-        Résultats de la recherche
-    </h1>
-    <?php if (have_posts()): ?>
-    <?php while (have_posts()) : the_post(); ?>
-    <article class="site__main__recherche">
-        <h3>
-            <a href="<?php echo get_permalink(); ?>">
-                <?php the_title(); ?>
-            </a>
-        </h3>
-        <p><?php echo wp_trim_words(get_the_content(),20) ?></p>
-        <hr>
-    </article>
-    <?php endwhile ?>
-    <?php endif ?>
+    <h1 class="site__main__titre">Résultat de la recherche</h1>
+    <section class="resultats-recherche">
+        <?php if (have_posts()) : while(have_posts()) : the_post() ?>
+            <article class="resultat-recherche">
+                <a href="<?= get_permalink(); ?>">
+                    <h3 class="resultat-recherche__titre"><?php the_title(); ?></h3>
+                </a>
+            </article>
+        <?php endwhile; endif; ?>
+    </section>
 </main>
-<?php get_footer() ?>;
+<?php get_footer(); ?>
