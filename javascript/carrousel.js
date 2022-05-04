66fa85b2cd5c42a68d4b5d1f9a7bc7966cdc784e
Danishcar Casimir <Danishcarcasimir25@gmail.com>
2022-05-04 17:17:11 -0400

    ajout des boutons pour la navigation du carrousel

--- javascript/carrousel.js
+++ javascript/carrousel.js
@@ -2,15 +2,20 @@
     console.log("vive la boîte carrousel")  
     let boite__carrousel = document.querySelector(".boite__carrousel")
     let galerie__img = document.querySelectorAll('.galerie .wp-block-image img')
+    let boite__carrousel__navigation = document.querySelector(".boite__carrousel__navigation")
     console.log("galerie__img.length =" + galerie__img.length)  
 
     //création de la balise image
     let image = document.createElement('img')
+    image.classList.add('boite__carrousel__img')
     boite__carrousel.appendChild(image)
     // image.setAttribute("src", '#')
 
-
+    let indexe = 0;
     for (const img of galerie__img) {
+        let bouton = document.createElement('button')
+        bouton.dataset.indexe = indexe++
+        boite__carrousel__navigation.append(bouton)
         console.log(img.tagName)
         img.addEventListener('mousedown',function(){
           boite__carrousel.classList.add('ouvrir')
